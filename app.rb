c7b64b5fac72e93b4d544081dfe0ef0b4451626f
Borja Martin Fernandez <bmartinf@axpe.com>
2013-12-09 15:43:15 +0100

    Scaffold heros

--- app.rb
+++ app.rb
@@ -1,13 +1,17 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  
   get '/' do
-    # use the views/index.erb file
     erb :index
   end
-
+  
   get '/agent' do
-    # use the views/agent.erb file
     erb :agent
   end
+  
+  get '/hero' do
+    erb :hero
+  end
+  
 end
