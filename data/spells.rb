b2c6488cf94590ee7d0800352c20c4b9beeb9d3f
Borja Martin Fernandez <bmartinf@axpe.com>
2013-12-19 01:22:30 +0100

    add few spells

--- data/spells.rb
+++ data/spells.rb
@@ -7,17 +7,71 @@ def spells
       :descripcion  => "Causa 2 dados de ataque por fuego."
     },
     { :id           => 2,
+      :name         => "Fuego de Ira",
+      :elemento     => "Fuego",
+      :nivel        => 1,
+      :descripcion  => "Causa 1 dano a un enemigo del tablero."
+    },
+    { :id           => 3,
+      :name         => "Valentia",
+      :elemento     => "Fuego",
+      :nivel        => 1,
+      :descripcion  => "+1 ataque de un aliado."
+    },
+    { :id           => 4,
+      :name         => "Genio",
+      :elemento     => "Aire",
+      :nivel        => 1,
+      :descripcion  => "Causa 2 dados de ataque por fuego."
+    },
+    { :id           => 5,
+      :name         => "Tempestad",
+      :elemento     => "Aire",
+      :nivel        => 1,
+      :descripcion  => "Causa 1 dano a un enemigo del tablero."
+    },
+    { :id           => 6,
+      :name         => "Rafaga",
+      :elemento     => "Aire",
+      :nivel        => 1,
+      :descripcion  => "+1 ataque de un aliado."
+    },
+    { :id           => 7,
+      :name         => "Piel de piedra",
+      :elemento     => "Tierra",
+      :nivel        => 1,
+      :descripcion  => "Causa 2 dados de ataque por fuego."
+    },
+    { :id           => 8,
+      :name         => "Atravesar muros",
+      :elemento     => "Tierra",
+      :nivel        => 1,
+      :descripcion  => "Causa 1 dano a un enemigo del tablero."
+    },
+    { :id           => 9,
+      :name         => "Cura corporal",
+      :elemento     => "Tierra",
+      :nivel        => 1,
+      :descripcion  => "+1 ataque de un aliado."
+    },
+    { :id           => 10,
+      :name         => "Niebla",
+      :elemento     => "Agua",
+      :nivel        => 1,
+      :descripcion  => "Causa 2 dados de ataque por fuego."
+    },
+    { :id           => 11,
       :name         => "Agua que cura",
       :elemento     => "Agua",
       :nivel        => 1,
-      :descripcion  => "Cura 4 puntos de cuerpo."
+      :descripcion  => "Causa 1 dano a un enemigo del tablero."
     },
-    { :id           => 3,
-      :name         => "Flechas de la noche",
-      :elemento     => "Sombras",
+    { :id           => 12,
+      :name         => "Dormir",
+      :elemento     => "Agua",
       :nivel        => 1,
-      :descripcion  => "Un ataque de 3 dados de sombra."
-    }
+      :descripcion  => "Duerme al objetivo."
+    },
   ]
 end
 
