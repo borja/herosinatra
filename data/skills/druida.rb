96cee5256c4e762eef4b24f81ab99eeae2c22d5c
Borja Martin Fernandez <bmartinf@axpe.com>
2013-12-23 16:31:03 +0100

    Add druid skills without operations

---
+++ data/skills/druida.rb
@@ -0,0 +1,14 @@
+def druidicas
+  [
+    {
+      :id           => 1,
+      :name         => "Espiritu Menor",
+      :description  => "Invoca un espritu guardian de nivel 1.",
+      :type         => "Activa"
+    },
+  ]
+end
+
+def druida(id)
+  druidicas[id-1]
+end
