3973db0e4b3ec72a554a7d070007fa8cc06a1b82
Iris <iris.gal.ser@gmail.com>
2013-12-23 16:59:25 +0100

    update protecciones: brazales, cinta, corona

--- data/protecciones.rb
+++ data/protecciones.rb
@@ -35,6 +35,27 @@ def protecciones
       :ranuras      => 2,
       :gemas        => []
     },
+    { :id           => 6,
+      :name         => "corona",
+      :categoria    => "metal",
+      :powa         => 1,
+      :ranuras      => 6,
+      :gemas        => []
+    },
+    { :id           => 7,
+      :name         => "cinta",
+      :categoria    => "tela",
+      :powa         => 0,
+      :ranuras      => 1,
+      :gemas        => []
+    },
+    { :id           => 8,
+      :name         => "brazales",
+      :categoria    => "tela",
+      :powa         => 1,
+      :ranuras      => 2,
+      :gemas        => []
+    },
   ]
 end
 
