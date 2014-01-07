0cc94fc63710109a72f6c2675ed95356069ef8d2
Iris <iris.gal.ser@gmail.com>
2014-01-07 23:04:23 +0100

    update skills: hechizar & hechiceria elfica

--- data/skills.rb
+++ data/skills.rb
@@ -34,7 +34,19 @@ def skills
       :name         => "Pacto de Honor",
       :description  => "1 PM: +1 de defensa azul este turno",
       :type         => "Activa"      
-    }     
+    },   
+    {
+      :id           => 6,
+      :name         => "Hechizar",
+      :description  => "El heroe tiene conocimientos sobre hechiceria y puede ejecutarla",
+      :type         => "Pasiva"      
+    },
+    {
+      :id           => 6,
+      :name         => "Hechiceria elfica",
+      :description  => "El heroe tiene conocimientos sobre hechiceria elfica y puede ejecutarla",
+      :type         => "Pasiva"      
+    },
   ]
 end
 
