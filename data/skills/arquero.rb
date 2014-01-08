f99a62807d5fc126b52103c82498f95e7f5a8d68
Iris <iris.gal.ser@gmail.com>
2014-01-08 20:05:47 +0100

    update arquero: cambia la descripción!!

--- data/skills/arquero.rb
+++ data/skills/arquero.rb
@@ -12,6 +12,66 @@ def arqueros
       :description  => "Permite disparar en cuerpo a cuerpo",
       :type         => "Pasiva"
     },
+    {
+      :id           => 3,
+      :name         => "Disparo arcano",
+      :description  => "Permite disparar en cuerpo a cuerpo",
+      :type         => "Pasiva"
+    },
+    {
+      :id           => 4,
+      :name         => "Disparo furtivo",
+      :description  => "Permite disparar en cuerpo a cuerpo",
+      :type         => "Pasiva"
+    },
+    {
+      :id           => 5,
+      :name         => "Disparo firme",
+      :description  => "Permite disparar en cuerpo a cuerpo",
+      :type         => "Pasiva"
+    },
+    {
+      :id           => 6,
+      :name         => "Disparo multiple",
+      :description  => "Permite disparar en cuerpo a cuerpo",
+      :type         => "Pasiva"
+    },
+    {
+      :id           => 7,
+      :name         => "Dominio del arco",
+      :description  => "Permite disparar en cuerpo a cuerpo",
+      :type         => "Pasiva"
+    },
+    {
+      :id           => 8,
+      :name         => "Disparo de rebote",
+      :description  => "Permite disparar en cuerpo a cuerpo",
+      :type         => "Pasiva"
+    },
+    {
+      :id           => 9,
+      :name         => "Disparo encantado",
+      :description  => "Permite disparar en cuerpo a cuerpo",
+      :type         => "Pasiva"
+    },
+    {
+      :id           => 10,
+      :name         => "Lluvia de flechas",
+      :description  => "Permite disparar en cuerpo a cuerpo",
+      :type         => "Pasiva"
+    },
+    {
+      :id           => 11,
+      :name         => "Fuego amigo",
+      :description  => "Permite disparar en cuerpo a cuerpo",
+      :type         => "Pasiva"
+    },
+    {
+      :id           => 12,
+      :name         => "Cargar disparo",
+      :description  => "Permite disparar en cuerpo a cuerpo",
+      :type         => "Pasiva"
+    },
   ]
 end
 
