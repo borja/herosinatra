620a576cd989f7348d7158bc00733fcdf42c6dc3
Iris <iris.gal.ser@gmail.com>
2014-01-30 17:49:18 +0100

    update piezas: id -1 --> id

--- data/items/utiles/piezas.rb
+++ data/items/utiles/piezas.rb
@@ -1,30 +1,30 @@
 # encoding: UTF-8
 def piezas
   [
-    { :id           => 1,
+    { :id           => 0,
       :name         => "llave",
     },
-    { :id           => 2,
+    { :id           => 1,
       :name         => "mapa",
     },
-    { :id           => 3,
+    { :id           => 2,
       :name         => "documento",
     },
-    { :id           => 4,
+    { :id           => 3,
       :name         => "herramientas vs trampas",
     },
-    { :id           => 5,
+    { :id           => 4,
       :name         => "caja de gemas",
     },
-    { :id           => 6,
+    { :id           => 5,
       :name         => "antorcha",
     },
-    { :id           => 7,
+    { :id           => 6,
       :name         => "estuche de pergaminos",
     },
   ]
 end
 
 def pieza(id)
-  piezas[id-1]
+  piezas[id]
 end
