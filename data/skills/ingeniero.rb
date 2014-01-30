dd3c5ee530f55b2ec5ddffa23a0f47996ca7299c
Iris <iris.gal.ser@gmail.com>
2014-01-30 17:19:28 +0100

    update ingeniero: skills

--- data/skills/ingeniero.rb
+++ data/skills/ingeniero.rb
@@ -12,7 +12,31 @@ def ingenieros
       :name         => "chatarrero",
       :description  => "Permite obtener una pieza al desactivar satisfactoriamente una trampa",
       :type         => "Pasiva"
-    }
+    },
+    {
+      :id           => 2,
+      :name         => "desvalijar",
+      :description  => "Permite obtener una pieza de las puertas.",
+      :type         => "activa"
+    },
+    {
+      :id           => 3,
+      :name         => "fuego amigo",
+      :description  => "Permite disparar a través de aliados sin dañarlos.",
+      :type         => "pasiva"
+    },
+    {
+      :id           => 4,
+      :name         => "retroceso",
+      :description  => "El exceso de potencia hará que el ingeniero retroceda, pudiendo golpear al posible enemigo con el que se choque.",
+      :type         => "pasiva"
+    },
+    {
+      :id           => 5,
+      :name         => "perforante",
+      :description  => "El exceso de daño que no se pueda parar un enemigo, le atravesará, yendo el disparo hacia otro enemigo que esté adyacente.",
+      :type         => "pasiva"
+    },
   ]
 end
 
