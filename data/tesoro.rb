a94de46e0ca85e24f9141b3225197e55ea5ecf81
Borja Martin Fernandez <bmartinf@axpe.com>
2014-02-17 10:13:42 +0100

    Add resistencias to ficha (still all empty)

--- data/tesoro.rb
+++ data/tesoro.rb
@@ -84,10 +84,7 @@ def cofre(sigmar)
     when 'comet'  then 'artefacto'
   end
 end
-  
-  
-  
-
+2
 def tesoro(resultado,sigmar)
   valores = ['pifia','vacia','vacia','comida','perg','oro',"monstruo",'pot','trampa', "caja",'cofre']
   return send(valores[resultado-2],sigmar)
