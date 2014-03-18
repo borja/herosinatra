c4d7cb55a27a1591d2f6693b3d753c8cfa134da7
Iris <iris.gal.ser@gmail.com>
2014-03-18 13:38:38 +0100

    update miscelaneas: add amuleto de melar

--- data/items/miscelaneas.rb
+++ data/items/miscelaneas.rb
@@ -61,6 +61,11 @@ def miscelaneas
       :name         => "saco garrapato",
       :max          => 1
     },
+    {
+      :id           => 13,
+      :name         => "amuleto de melar",
+      :max          => 1
+    },
   ]
 end
 
