6d2b0d9dee7d4e5da7d7f4e3fd26fd0c225f01d2
Borja Martin Fernandez <bmartinf@axpe.com>
2014-03-19 17:32:52 +0100

    Fix especializaciones

--- data/skills/especializaciones.rb
+++ data/skills/especializaciones.rb
@@ -1,15 +1,16 @@
 # encoding: UTF-8
-def especializacione
+def especializacions
   [
     {
-      :id => 0,
-      :name => "especialista",
+      :id          => 0,
+      :name        => "especialista",
       :description => "El hechicero se especializa en 1 elemento, obteniendo +1POT en los hechizos de ese elemento. A cambio, tendrá una penalización en 2 elementos de -1 POT en sus hechizos.",
-      :type => "pasiva",
+      :type        => "pasiva",
+      :coste       => 100,
     },
   ]
 end
 
-def especializacione(id)
-  especializaciones[id]
+def especializacion(id)
+  especializacions[id]
 end
