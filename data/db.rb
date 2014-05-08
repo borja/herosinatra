eb5eb14575c0a92b8e53ea5b510ee6207a59b5b4
U-Borja-PC\Borja <Borja@Borja-PC.(none)>
2014-05-08 22:21:38 +0200

    Create fake db file in data

---
+++ data/db.rb
@@ -0,0 +1,13 @@
+# Fake Data Base
+require './data/heros'
+require './data/menu'
+require './data/spells'
+require './data/skills'
+require './data/profesiones'
+require './data/chars'
+require './data/pets'
+require './data/resistencias'
+require './data/ficha'
+require './data/tesoro'
+require './data/items'
+require './data/criaturas/'
