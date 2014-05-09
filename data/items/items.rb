c4fae6dfa2c645e91103b7b6810a8f742e5dee1e
Borja Martín Fernández <bmartinf@axpe.com>
2014-05-10 00:53:18 +0200

    refactor db, add potencia to spells in data

--- data/items/items.rb
+++ data/items/items.rb
@@ -1,21 +1,15 @@
 # encoding: UTF-8
 
-# Item categories:
-def espalda_lista     ; [1]                 end
-def cuello_lista      ; [2,9]               end
-def anillo_lista      ; [1,11]              end
-def pies_lista        ; [3]                 end
-def cintura_lista     ; [4,11]              end
-def muñecas_lista     ; [8]                 end
-def hombros_lista     ; [5]                 end
-def cabeza_lista      ; [2,6,12,13,15]      end
-def pechera_lista     ; [9,14]              end
-def guantes_lista     ; [10]                end
-def hebilla_lista     ; [0]                 end
-def tobillos_lista    ; [14]                end
-def argolla_lista     ; [4]                 end
-def pendientes_lista  ; [3]                 end
-def tatuajes_lista    ; [15]                end
-def cadena_lista      ; [5]                 end
-def broche_lista      ; [7]                 end
-def entrepierna_lista ; [0]                 end
+# Items Data Base
+require './data/items/miscelaneas'
+require './data/items/enchants'
+require './data/items/engarces/gemas'
+require './data/items/engarces/joyas'
+require './data/items/engarces/runas'
+require './data/items/engarces/tiers'
+require './data/items/utiles/piezas'
+require './data/items/utiles/pergaminos'
+require './data/items/utiles/pociones'
+require './data/items/armas'
+require './data/items/armaduras'
+require './data/items/protecciones'
