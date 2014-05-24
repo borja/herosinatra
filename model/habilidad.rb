ecbb1a435a1fd4129feb047a149fe1c18dad73c9
Borja Martín Fernández <bmartinf@axpe.com>
2014-05-24 02:05:20 +0200

    Fix habilidad model

--- model/habilidad.rb
+++ model/habilidad.rb
@@ -3,4 +3,11 @@
 
 class Habilidad < Hash
   attr_accessor :id, :name, :description, :type
+  
+  def initialize args
+    args.each do |k,v|
+      instance_variable_set("@#{k}".to_sym, v) unless v.nil?
+    end
+  end
+  
 end
