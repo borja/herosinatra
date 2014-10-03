3552a33444a4c97ff46518e4958273763f82884e
Iris Galeano Serrano <iris.gal.ser@gmail.com>
2014-10-03 12:19:00 +0200

    Add ingeniero imgs and skills

--- data/skills/ingeniero.rb
+++ data/skills/ingeniero.rb
@@ -9,13 +9,13 @@ def ingenieros
     },
     {
       :id           => 1,
-      :name         => "chatarrero",
+      :name         => "Chatarrero",
       :description  => "Permite obtener una pieza al desactivar satisfactoriamente una trampa",
       :type         => "Pasiva"
     },
     {
       :id           => 2,
-      :name         => "desvalijar",
+      :name         => "Desvalijar",
       :description  => "Permite obtener una pieza de las puertas.",
       :type         => "activa"
     },
@@ -27,43 +27,41 @@ def ingenieros
     },
     {
       :id           => 4,
-      :name         => "retroceso",
+      :name         => "Retroceso",
       :description  => "El exceso de potencia hará que el ingeniero retroceda, pudiendo golpear al posible enemigo con el que se choque.",
       :type         => "pasiva"
     },
     {
       :id           => 5,
-      :name         => "disparo perforante",
+      :name         => "Disparo perforante",
       :description  => "El exceso de daño que no se pueda parar un enemigo, le atravesará, yendo el disparo hacia otro enemigo que esté adyacente.",
       :type         => "pasiva"
     },
     {
       :id           => 6,
-      :name         => "éter",
+      :name         => "Éter",
       :description  => "Por describir.",
       :type         => "activa"
     },
     {
       :id           => 7,
-      :name         => "cachivache",
+      :name         => "Cachivache",
       :description  => "A cambio de 3 piezas de éter creas un cachivache que puede hacer las siguientes funciones: autodestrucción, +1 a buscar si se coloca en el casco,  permite disparo oblicuo si se coloca en la mirilla.",
       :type         => "pasiva"
     },
     {
       :id           => 8,
-      :name         => "diógenes",
+      :name         => "Diógenes",
       :description  => "Si tienes éxito, no gastas piezas.",
       :type         => "pasiva"
     },
     {
       :id           => 9,
-      :name         => "disparo doble",
+      :name         => "Disparo doble",
       :description  => "Permite disparar 2 veces.",
       :type         => "pasiva"
     },
   ]
 end
 
-def ingeniero(id)
-  ingenieros[id]
-end
+def ingeniero id ; ingenieros[id] end
