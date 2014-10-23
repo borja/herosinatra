8dddd5c0c767f5b84424eb7906bc080c78c6a7ae
Borja Martín Fernández <borjamartinfernandez@gmail.com>
2014-10-23 09:47:56 +0200

    First scaffold of resistencias

--- data/resistencias.rb
+++ data/resistencias.rb
@@ -1,18 +1,10 @@
 # encoding: UTF-8
 
-def resistencia(id)
-  total = {
-    :calor    => 0,
-    :psionico => 0,
-    :escarcha => 0,
-    :miedo    => 0,
-    :tenebro  => 0,
-    :peste    => 0,
-    :rayos    => 0,
-  }
-  return total
+def resistencia(res)
+  case res
+    when 'calor' then return "Hace calor"
+    else return 'no lo hace'
+  end
 end
 
-def resistencias
-  ["psionico", "calor", "escarcha", "miedo", "peste", "rayos","tenebro"]
-end
+def resistencias ; ["psionico", "calor", "escarcha", "miedo", "peste", "rayos","tenebro"] end
