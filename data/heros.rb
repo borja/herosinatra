6b99e1eadeaff6a2a185b6582e6257861d42e0ae
Borja Martín Fernández <borjamartinfernandez@gmail.com>
2015-01-03 18:31:58 +0100

    Delete torpeza in limoncito :)

--- data/heros.rb
+++ data/heros.rb
@@ -358,7 +358,7 @@ def heros
       :miscelaneas  => [Miscelanea.new({:id => 11})],
       :skills       => [0,1,2,4,5,6,8],
       :profesion    => Profesion.new({:id => 2,  :aprendiz => [0,1]}),  
-      :pet          => Pet.new({:id  => 19, :name => "Limoncito", :torpe => true}),
+      :pet          => Pet.new({:id  => 19, :name => "Limoncito"}),
       :pociones     => [6,0],
       :piezas       => [0,4],
       :pergaminos   => [Pergamino.new({:id => 1, :spells   => [11] })],
