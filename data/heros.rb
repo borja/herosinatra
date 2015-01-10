397be02b2db2eee0fd857e354b6c316dde0750dd
Borja Martín Fernández <borjamartinfernandez@gmail.com>
2015-01-10 13:23:33 +0100

    changes in Favule

--- data/heros.rb
+++ data/heros.rb
@@ -899,7 +899,7 @@ def heros
     }),
     Hero.new({ 
       :id           => 46,
-      :name         => "Vowie Favule",
+      :name         => "Gracia Favule",
       :personaje    => "derviche",
       :jugador      => "Daniel Cabañas",
       :status       => "reserva",
@@ -909,6 +909,7 @@ def heros
       :mov          => 8,
       :armas        => [Arma.new({:id => 1})],
       :armadura     => Armadura.new({:id => 2}),
+      :pet          => Pet.new({:id => 15, :name => "Fabulosa", :torpe => true}),
       :hechizos     => [18,19,20],
     }),
     Hero.new({ 
