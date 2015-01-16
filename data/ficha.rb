7ab7c5ab53570ab8542e9c8e277928b4764c2403
Borja Martín Fernández <borjamartinfernandez@gmail.com>
2015-01-16 18:28:09 +0100

    Fix piés != pies bug

--- data/ficha.rb
+++ data/ficha.rb
@@ -4,7 +4,7 @@ def lore        ;["rostro", "héroe", "raza", "pareja"]                         
 def disciplinas ;["id", "icono", "tipo", "nombre", "descripción", "nivel", "requisitos"] end
 def engarces    ;["gemas","joyas","runas"]                                               end
 def habs        ;["icono", "tipo", "nombre", "descripción"]                              end
-def fields      ;{ 1 => ["cabeza","hombros","muñecas","cintura","piés"],
+def fields      ;{ 1 => ["cabeza","hombros","muñecas","cintura","pies"],
                    2 => ["cuello","espalda","pechera","manos","hebilla","tobillos"],
                    3 => ["argolla","pendientes","tatuajes","cadena","broche","entrepierna"],
                  } end 
